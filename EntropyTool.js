9661a64c5a6d9624cb53cef19e1fff417f7ef014
Richard Alex Hofer <atomic.quark@gmail.com>
2014-10-24 17:46:21 -0400

    Fixed errors found by JSLint.

--- EntropyTool.js
+++ EntropyTool.js
@@ -1,16 +1,17 @@
+(function(){
 "use strict";
 var EntropyMadeVisible = (function(my) {
+  var increaseN = function increaseN() {
+    var n = Math.sqrt($("#JointProbGrid").cells.length);
+    console.log(n);
+  };
+
   my.makeTool = function makeTool(id) {
     $("#" + id).load("Tool.html");
     increaseN();
-  }
-
-  function increaseN() {
-    var n = Math.sqrt($("#JointProbGrid").cells.length);
-    console.log(n);
-  }
+  };
 
   return my;
 }({}));
-console.log("TESTING");
-x = 8;
+console.log("Loaded EntropyTool.js");
+}());
