ae9ec3f232e5e2b80bba8255992334cb6dc23fdd
Richard Alex Hofer <atomic.quark@gmail.com>
2014-10-24 17:11:38 -0400

    Minor refactoring.

--- EntropyTool.js
+++ EntropyTool.js
@@ -1,10 +1,15 @@
 "use strict";
-function makeTool(id) {
-	$("#" + id).load("Tool.html");
-	increaseN();
-}
+var EntropyMadeEasy = (function(my) {
+  my.makeTool = function makeTool(id) {
+    $("#" + id).load("Tool.html");
+    increaseN();
+  }
 
-function increaseN() {
-	var n = Math.sqrt($("#JointProbGrid").cells.length);
-	console.log(n);
-}
+  function increaseN() {
+    var n = Math.sqrt($("#JointProbGrid").cells.length);
+    console.log(n);
+  }
+
+  return my;
+}({}));
+console.log("TESTING");
